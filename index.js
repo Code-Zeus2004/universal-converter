7cd240dd5e6d836d080c914b8b48fc1ccac2172d
Code-Zeus <samiqzuu1991@gmail.com>
2026-05-23 01:13:55 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("form");
+const amountInput = document.getElementById("amount");
+const fromSelect = document.getElementById("From");
+const toSelect = document.getElementById("To");
+const submitButton = document.getElementById("submit");
+const resultDisplay = document.getElementById("result");
+const header = document.getElementById("header");
+
